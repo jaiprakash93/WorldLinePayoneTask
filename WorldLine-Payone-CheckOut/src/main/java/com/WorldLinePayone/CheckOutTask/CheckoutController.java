6ee6f69f1be1debb834b5c8360a239b8639a8f97
jaiprakash93 <30869208+jaiprakash93@users.noreply.github.com>
2024-10-06 23:06:57 +0530

    Update CheckoutController.java

--- WorldLine-Payone-CheckOut/src/main/java/com/WorldLinePayone/CheckOutTask/CheckoutController.java
+++ WorldLine-Payone-CheckOut/src/main/java/com/WorldLinePayone/CheckOutTask/CheckoutController.java
@@ -1,5 +1,19 @@
 package com.WorldLinePayone.CheckOutTask;
 
+@RestController
+@RequestMapping("/checkout")
 public class CheckoutController {
 
+    @Autowired
+    private CheckoutService checkoutService;
+
+    @PostMapping("/scan")
+    public void scanItem(@RequestBody String item) {
+        checkoutService.scan(item);
+    }
+
+    @GetMapping("/total")
+    public int getTotalPrice() {
+        return checkoutService.total();
+    }
 }
