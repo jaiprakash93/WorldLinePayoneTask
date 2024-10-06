741fb9a5aeadb5f444a3cc6d95db8f3cd86fcd61
jaiprakash93 <30869208+jaiprakash93@users.noreply.github.com>
2024-10-06 23:10:08 +0530

    Update PricingController.java

--- WorldLine-Payone-Pricing/src/main/java/com/WorldLinePayone/Pricing/PricingController.java
+++ WorldLine-Payone-Pricing/src/main/java/com/WorldLinePayone/Pricing/PricingController.java
@@ -1,5 +1,13 @@
 package com.WorldLinePayone.Pricing;
 
+@RestController
 public class PricingController {
 
+    @Autowired
+    private PricingService pricingService;
+
+    @GetMapping("/price")
+    public int getPrice(@RequestParam String item, @RequestParam int quantity) {
+        return pricingService.getPrice(item, quantity);
+    }
 }
